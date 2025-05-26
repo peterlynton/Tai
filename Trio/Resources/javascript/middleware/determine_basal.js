a425f0730c871182b1f43d8a2730d738ce320eb7
Robert <comps@diprobe.com>
2025-05-26 12:00:38 +0200

    provide glucose to Middleware

--- Trio/Resources/javascript/middleware/determine_basal.js
+++ Trio/Resources/javascript/middleware/determine_basal.js
@@ -1,5 +1,13 @@
-function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory, preferences, basalprofile) {
+function middleware(iob, currenttemp, glucose_status, profile, autosens, meal, reservoir, clock, pump_history, preferences, basalProfile, oref2_variables) {
+    
     // modify anything
     // return any reason what has changed.
+
+    const currentBG = glucose_status.glucose;
+    const trend = glucose_status.delta;
+    const d = new Date();
+    let currentHour = d.getHours();
+    
     return "Nothing changed";
+
 }

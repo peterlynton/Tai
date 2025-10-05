f3e440ee38d3285062f538ee360e39dc9662034d
Robert <comps@diprobe.com>
2025-10-05 16:59:18 +0200

    adapt GarminWatchSTate model to match swissalpine

--- Trio/Sources/Models/GarminWatchState.swift
+++ Trio/Sources/Models/GarminWatchState.swift
@@ -2,49 +2,61 @@ import Foundation
 import SwiftUI
 
 struct GarminWatchState: Hashable, Equatable, Sendable, Encodable {
-    var glucose: String?
-    var trendRaw: String?
-    var delta: String?
-    var iob: String?
-    var cob: String?
-    var lastLoopDateInterval: UInt64?
-    var eventualBGRaw: String?
-    var isf: String?
-    var sensRatio: String?
+    var date: UInt64?
+    var sgv: Int?
+    var delta: Double?
+    var direction: String?
+    var noise: Double?
+    var units_hint: String?
+    var iob: Double?
+    var tbr: Int?
+    var cob: Double?
+    var eventualBG: Int?
+    var isf: Int?
+    var sensRatio: Double?
 
     static func == (lhs: GarminWatchState, rhs: GarminWatchState) -> Bool {
-        lhs.glucose == rhs.glucose &&
-            lhs.trendRaw == rhs.trendRaw &&
+        lhs.date == rhs.date &&
+            lhs.sgv == rhs.sgv &&
             lhs.delta == rhs.delta &&
+            lhs.direction == rhs.direction &&
+            lhs.noise == rhs.noise &&
+            lhs.units_hint == rhs.units_hint &&
             lhs.iob == rhs.iob &&
+            lhs.tbr == rhs.tbr &&
             lhs.cob == rhs.cob &&
-            lhs.lastLoopDateInterval == rhs.lastLoopDateInterval &&
-            lhs.eventualBGRaw == rhs.eventualBGRaw &&
+            lhs.eventualBG == rhs.eventualBG &&
             lhs.isf == rhs.isf &&
             lhs.sensRatio == rhs.sensRatio
     }
 
     func hash(into hasher: inout Hasher) {
-        hasher.combine(glucose)
-        hasher.combine(trendRaw)
+        hasher.combine(date)
+        hasher.combine(sgv)
         hasher.combine(delta)
+        hasher.combine(direction)
+        hasher.combine(noise)
+        hasher.combine(units_hint)
         hasher.combine(iob)
+        hasher.combine(tbr)
         hasher.combine(cob)
-        hasher.combine(lastLoopDateInterval)
-        hasher.combine(eventualBGRaw)
+        hasher.combine(eventualBG)
         hasher.combine(isf)
         hasher.combine(sensRatio)
     }
 
     // Custom encoding to exclude nil values
     enum CodingKeys: String, CodingKey {
-        case glucose
-        case trendRaw
+        case date
+        case sgv
         case delta
+        case direction
+        case noise
+        case units_hint
         case iob
+        case tbr
         case cob
-        case lastLoopDateInterval
-        case eventualBGRaw
+        case eventualBG
         case isf
         case sensRatio
     }
@@ -52,15 +64,17 @@ struct GarminWatchState: Hashable, Equatable, Sendable, Encodable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
 
-        try container.encodeIfPresent(glucose, forKey: .glucose)
-        try container.encodeIfPresent(trendRaw, forKey: .trendRaw)
+        try container.encodeIfPresent(date, forKey: .date)
+        try container.encodeIfPresent(sgv, forKey: .sgv)
         try container.encodeIfPresent(delta, forKey: .delta)
+        try container.encodeIfPresent(direction, forKey: .direction)
+        try container.encodeIfPresent(noise, forKey: .noise)
+        try container.encodeIfPresent(units_hint, forKey: .units_hint)
         try container.encodeIfPresent(iob, forKey: .iob)
+        try container.encodeIfPresent(tbr, forKey: .tbr)
         try container.encodeIfPresent(cob, forKey: .cob)
-        try container.encodeIfPresent(lastLoopDateInterval, forKey: .lastLoopDateInterval)
-        try container.encodeIfPresent(eventualBGRaw, forKey: .eventualBGRaw)
+        try container.encodeIfPresent(eventualBG, forKey: .eventualBG)
         try container.encodeIfPresent(isf, forKey: .isf)
-        // sensRatio will only be encoded if it's not nil
         try container.encodeIfPresent(sensRatio, forKey: .sensRatio)
     }
 }
